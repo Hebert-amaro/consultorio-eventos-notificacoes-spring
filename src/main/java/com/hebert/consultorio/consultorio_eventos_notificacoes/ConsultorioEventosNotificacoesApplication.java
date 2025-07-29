44c594c79696a32193007cf6ad22dd292c4f4b0b
Hebert-amaro <hebert.amaro.araujo.quirino@ccc.ufcg.edu.br>
2025-07-28 22:23:41 -0300

    feat: Configurando ambiente

---
+++ src/main/java/com/hebert/consultorio/consultorio_eventos_notificacoes/ConsultorioEventosNotificacoesApplication.java
@@ -0,0 +1,13 @@
+package com.hebert.consultorio.consultorio_eventos_notificacoes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConsultorioEventosNotificacoesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConsultorioEventosNotificacoesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hebert/consultorio/consultorio_eventos_notificacoes/ConsultorioEventosNotificacoesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hebert.consultorio.consultorio_eventos_notificacoes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConsultorioEventosNotificacoesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
